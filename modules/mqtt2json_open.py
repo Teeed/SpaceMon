ed0b0290d71a747349d2c0f2d2efe8aaa5b8049f
Tadeusz Magura-Witkowski <tadeuszmw@gmail.com>
2021-11-26 21:23:49 +0100

    Added lost module (mqtt2json)

---
+++ modules/mqtt2json_open.py
@@ -0,0 +1,28 @@
+__version__ = 1.0
+__cacheable__ = True
+
+import urllib.request, urllib.error, urllib.parse
+import json
+import time
+from application import config
+
+
+CONFIG_KEY = 'module_mqtt2json_open'
+
+def update_document(data):
+	remote = urllib.request.urlopen(config.get(CONFIG_KEY, 'url'), timeout=config.getint(CONFIG_KEY, 'timeout')).read()
+	remote = json.loads(remote)
+
+	if remote['zm/hs_active']['timestamp'] < time.time() - 120:
+		return
+
+	is_open = remote['zm/hs_active']['payload'] == 1
+	
+	if not data.get('sensors'):
+		data['sensors'] = {}
+
+		if not data.get('state'):
+			data['state'] = {}
+
+		data['state']['open'] = is_open
+
